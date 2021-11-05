735782ee00267c4229ab61debb34509b8b44b231
Wilmela <chukumelawilson@gmail.com>
2021-11-05 10:14:41 +0100

    Completed project

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from "@angular/core";
-import { ProductService } from "../product.service";
-import { Album } from "../album";
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Album } from '../album';
 @Component({
-  selector: "app-product-description",
-  templateUrl: "./product-description.component.html",
-  styleUrls: ["./product-description.component.css"],
+  selector: 'app-product-description',
+  templateUrl: './product-description.component.html',
+  styleUrls: ['./product-description.component.css'],
 })
 export class ProductDescriptionComponent implements OnInit {
   albumInfo: Album;
@@ -15,5 +15,4 @@ export class ProductDescriptionComponent implements OnInit {
       .getAlbum(1)
       .subscribe(response => this.albumInfo = response);
   }
-
 }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from "@angular/core";
-import { Product } from "../product";
-import { ProductService } from "../product.service";
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { ProductService } from '../product.service';
 
 @Component({
-  selector: "app-product-list",
-  templateUrl: "./product-list.component.html",
-  styleUrls: ["./product-list.component.css"],
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
   products: Product[];
